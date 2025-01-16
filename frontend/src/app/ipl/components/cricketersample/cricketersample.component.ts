a207abb07ef241506e63d277f0c9ca64a1c44884
username <wecp-dummy@gmail.com>
2025-01-16 11:55:02 +0000

    day17

--- frontend/src/app/ipl/components/cricketersample/cricketersample.component.ts
+++ frontend/src/app/ipl/components/cricketersample/cricketersample.component.ts
@@ -1,4 +1,12 @@
-
+import { Component } from "@angular/core";
+import { Cricketer } from "../../types/Cricketer";
+@Component({
+    selector: 'app-crickersample',
+    standalone: true,
+    imports: [],
+    templateUrl: './cricketersample.component.html',
+    styleUrls: ['./cricketersample.component.scss']
+})
 export class CricketerSampleComponent {
-  
+  cricketer: Cricketer = new Cricketer(1, 1, "Virat", 32, "Indian", 14, "Batsman", 580, 50);
 }
